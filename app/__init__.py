74d51d7ae32ae2e641ff2443f58f2ab27dd0400b
VGabbasov <VGabbasov@inno.tech>
2025-10-31 11:25:37 +0300

    feat: Убрал тестовое сообщение

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,6 @@ from logging.handlers import QueueHandler, QueueListener
 import os
 import functools
 import logging
-import threading
-import time
 from app.config import logging_level, logs_directory
 from app.tslg_logging import TSLGBufferedSocketHandler, TSLGJSONLogFormatter
 
@@ -107,19 +105,5 @@ app.logger.info(f"Buffer size: {os.getenv('TSLG_MAX_BUFFER_SIZE')}, Flush interv
 # To allow flask propagating exception even if debug is set to false on integration_services
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def start_test_message_sender():
-    """Запуск потока для отправки тестовых сообщений каждую секунду"""
-    def send_test_message():
-        while True:
-            time.sleep(1)
-            if os.getenv('TSLG_CONSOLE_OUTPUT', 'true').lower() not in ['false', '0', 'no']:
-                test_message = "Тестовое сообщение сервиса интеграции"
-                app.logger.info(test_message)
-
-    test_thread = threading.Thread(target=send_test_message, daemon=True)
-    test_thread.start()
-
-start_test_message_sender()
 
 from app import mlflow, bitbucket, nexus, jira, teamcity, upload, kafka_rest, s3_minio
